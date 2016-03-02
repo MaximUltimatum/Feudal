81b3f8e436d931268f7f6f215b304712d5153ab8
MaximUltimatum <mcder018@live.com>
2016-03-02 18:01:39 -0500

    Initial commit of stub class

---
+++ Feudal/src/feudalMain/FeudalOnline.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package feudalMain;
+
+/**
+ * @author Student
+ *
+ */
+public class FeudalOnline {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
